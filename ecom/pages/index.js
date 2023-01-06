0e2daa615ef228a82538902c68cd971265619e6f
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2023-01-06 23:16:28 +0000

    Displaying Products
    Displaying Products from Sanity.
    Also displaying information in the footer banner.

--- ecom/pages/index.js
+++ ecom/pages/index.js
@@ -10,9 +10,11 @@ const Home = ({ products, banner }) => {
         <p>Speakers of many variations</p>
       </div>
       <div className="products-container">
-        {products?.map((product) => product.name)}
+        {products?.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
       </div>
-      <FooterBanner />
+      <FooterBanner footerBanner={banner && banner[0]} />
     </>
   );
 };
