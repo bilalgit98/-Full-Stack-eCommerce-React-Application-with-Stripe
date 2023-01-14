e59cff63b88fa750ff1757958355c1482a361918
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2023-01-14 01:07:04 +0000

    Stripe Checkout Added

--- ecom/components/Cart.jsx
+++ ecom/components/Cart.jsx
@@ -9,11 +9,12 @@ import {
 
 import { TiDeleteOutline } from "react-icons/ti";
 
-import { Toast } from "react-hot-toast";
+import toast from "react-hot-toast";
 
 import { useStateContext } from "../context/StateContext";
 
 import { urlFor } from "../lib/client";
+import getStripe from "../lib/getStripe";
 
 const Cart = () => {
   const cartRef = useRef();
@@ -25,6 +26,25 @@ const Cart = () => {
     toggleCartItemQuantity,
     onRemove,
   } = useStateContext();
+
+  const handleCheckout = async () => {
+    const stripe = await getStripe();
+    const response = await fetch("/api/stripe", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(cartItems),
+    });
+
+    if (response.statusCode === 500) return;
+
+    const data = await response.json();
+    console.log(data);
+    toast.loading("Redirecting...");
+
+    stripe.redirectToCheckout({ sessionId: data.id });
+  };
 
   return (
     <div className="cart-wrapper" ref={cartRef}>
@@ -110,7 +130,7 @@ const Cart = () => {
               <h3>£{totalPrice}</h3>
             </div>
             <div className="btn-container">
-              <button type="button" className="btn" onClick="">
+              <button type="button" className="btn" onClick={handleCheckout}>
                 Proceed to checkout!
               </button>
             </div>
